2d1fab121bcb8342579ecfa2d696a3e3a094f53f
Daniel Bu <danielbu@Daniel-Bus-MBP.local>
2019-12-07 03:24:13 -0500

    test before dockerize

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ from app import app
 if __name__ == "__main__":
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        app.run(debug=True)
+        app.run(debug=False, host="0.0.0.0", port=8000)
